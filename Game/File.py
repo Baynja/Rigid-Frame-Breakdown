e14dbd9bc7d6aaca51b47ccf1db94b8715b00ac1
Baynja <baynjamin@gmail.com>
2025-03-24 15:36:33 +0700

    Update File.py

--- Game/File.py
+++ Game/File.py
@@ -1,6 +1,13 @@
 import pygame
+import pymunk
+import pymunk.pygame_util
+
 print(pygame.__version__)
 pygame.init()
+
+space = pymunk.Space()
+clock = pygame.time.Clock()
+FPS = 60
 
 #screen
 height = 720
@@ -8,16 +15,35 @@ width = 1024
 screen = pygame.display.set_mode((width, height))
 
 red_value, green_value, blue_value = 0,255,255
-pygame.display.set_caption("game")
+pygame.display.set_caption("Simulation")
 
-#display image
-def create_obj(img, imgpos):
-    screen.blit(img, imgpos)
+space = pymunk.Space()
+space.gravity = 0, -981  # Horizontal and vertical forces
 
-#display text
-def create_text(text, font, colour, coordinates):
-    text_img = font.render(text, False, colour)
-    screen.blit(text_img, coordinates)
+def Add_Circle(radius, pos, density):
+    body = pymunk.Body(body_type=pymunk.Body.DYNAMIC)
+    body.position = pos[0], pos[1]
+    shape = pymunk.Circle(body, radius)
+    shape.elasticity = 1
+    shape.density = density
+    space.add(body, shape)
+    return body, shape
+
+def Add_Bridge_Segment(posA, posB, segment_thickness):
+    body = pymunk.Body(body_type = pymunk.Body.STATIC)
+    shape = pymunk.Segment(body, posA, posB, segment_thickness)
+    shape.elasticity = 0.5
+    space.add(body, shape)
+    return body, shape
+
+
+def convert_coordinates(position):
+    return position[0], (height-position[1]) #position[0] is an x value, and pos[1] is the y val
+
+
+First_Body, First_Shape = Add_Circle(10, (width/2, height/2), 1)
+First_Segment, Segment_Shape = Add_Bridge_Segment((0,500), (width, 10), 10)
+print(convert_coordinates(Segment_Shape.b))
 
 #rendering window
 running_state = True
@@ -33,4 +59,13 @@ while running_state:
     #fills screen with colour
     screen.fill((red_value, green_value, blue_value))
 
+    x,y = convert_coordinates(First_Body.position)
+    pygame.draw.circle(screen, (255,255,0), (int(x), int(y)), 10)
+
+    x,y = convert_coordinates(Segment_Shape.a)
+    x1,y1 = convert_coordinates(Segment_Shape.b)
+    pygame.draw.line(screen, (0,0,0), (int(x), int(y)), (int(x1), int(y1)), 10)
+
     pygame.display.update()
+    clock.tick(FPS)
+    space.step(1/FPS)
